02869f21984a1cb373e9e95c96a7ecf556401c72
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2024-02-01 20:13:20 +0600

    file download

---
+++ src/download.rs
@@ -0,0 +1,42 @@
+use reqwest::{Client, Url};
+use std::io::copy;
+use std::{fs::File, path::PathBuf};
+
+pub async fn download_file() {
+    let saved_path = PathBuf::from("src");
+    let client = Client::builder()
+        .danger_accept_invalid_certs(true)
+        .build()
+        .unwrap();
+
+    match client
+        .get("http://127.0.0.1:49152/files/addr.rs")
+        .send()
+        .await
+    {
+        Ok(response) => {
+            let file_name = file_name(response.url().clone());
+            let mut dest = {
+                println!("file_name: {}", file_name);
+
+                println!("file to download: '{}'", file_name);
+                let fname = saved_path.join(file_name);
+                println!("will be located under: '{:?}'", fname);
+                File::create(fname).unwrap()
+            };
+            let content = response.text().await.unwrap();
+            copy(&mut content.as_bytes(), &mut dest).unwrap();
+        }
+        Err(e) => {
+            println!("Error: {}", e);
+        }
+    }
+}
+
+fn file_name(url: Url) -> String {
+    url.path_segments()
+        .and_then(|segments| segments.last())
+        .and_then(|name| if name.is_empty() { None } else { Some(name) })
+        .unwrap_or("tmp.bin")
+        .to_string()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use download::download_file;
+pub mod download;
+// use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
+// use std::net::TcpStream;
+
+#[tokio::main]
+async fn main() {
+    // let client = reqwest::Client::new();
+    // let connector = SslConnector::builder(SslMethod::tls())
+    //     .unwrap()
+    //     .verify_mode(SslVerifyMode::NONE)
+    //     .build();
+
+    // let stream = TcpStream::connect("127.0.0.1:49154").unwrap();
+    // let ssl_stream = connector.connect("127.0.0.1", stream).unwrap();
+
+    download_file().await
 }
